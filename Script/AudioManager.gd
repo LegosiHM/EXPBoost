ed450227a0495b5ba602001f8bb8dfb25b41669b
nmmn4947 <nmmn4947@gmail.com>
2024-07-08 01:47:21 +0700

    TestAudioManager

---
+++ Script/AudioManager.gd
@@ -0,0 +1,22 @@
+extends Node
+
+@onready var test_sound = $TestSound
+@onready var test_sound_2 = $TestSound2
+
+const DOOM_CUT = preload("res://NodeScenes/Audio/Doom Cut.mp3")
+const FLUFFING_A_DUCK = preload("res://NodeScenes/Audio/Fluffing a Duck.mp3")
+
+@onready var b_gmusic = $BGmusic
+
+func _ready():
+	b_gmusic.stream = DOOM_CUT
+	b_gmusic.play()
+
+func ChangeBGMusic():
+	if b_gmusic.stream == DOOM_CUT:
+		b_gmusic.stream = FLUFFING_A_DUCK
+	else:
+		b_gmusic.stream = DOOM_CUT
+		
+	b_gmusic.play()
+	
