dbad5478eac8499098e649e12e9fe232cb455e4a
nmmn4947 <nmmn4947@gmail.com>
2024-07-28 16:08:30 +0700

    MenuDone

---
+++ Menu.gd
@@ -0,0 +1,8 @@
+extends Control
+
+func _on_texture_button_2_pressed():
+	get_tree().quit() # Replace with function body.
+
+
+func _on_texture_button_pressed():
+	get_tree().change_scene_to_file("res://Main.tscn")
