3918fbc35919278785d46771560a48828bd6179f
LegosiHM <105794916+LegosiHM@users.noreply.github.com>
2024-08-01 21:15:26 +0700

    Game Balanced

--- Script/Character/Boss/Boss.gd
+++ Script/Character/Boss/Boss.gd
@@ -44,7 +44,7 @@ func despawn_laser():
 func _on_hurtbox_area_entered(hitbox):
 	if hitbox.is_in_group("PlayerBullet"):
 		currentHealth -= 1
-		DamageNumbers.display_number(100, $DamageNumber.global_position)
-		DamageNumbers.current_score += 100
+		DamageNumbers.display_number(50, $DamageNumber.global_position)
+		DamageNumbers.current_score += 50
 		healthbar.health = currentHealth
 		print(currentHealth)
